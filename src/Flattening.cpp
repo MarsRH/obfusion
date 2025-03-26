83f6c0271488a096b0089919e61a41c6d9bf30f0
WhitePeach <Elysia_awa@outlook.com>
2025-03-26 21:49:05 +0800

    ✨ update: 优化控制流平坦化逻辑，确保基本块有终止指令并处理不同返回类型

--- src/Flattening.cpp
+++ src/Flattening.cpp
@@ -32,8 +32,12 @@ PreservedAnalyses Flattening::run(Function &F, FunctionAnalysisManager &) {
   AllocaInst *switchVar = builder.CreateAlloca(
     builder.getInt32Ty(), nullptr, "switchVar");
 
-  // 设置初始值
-  builder.SetInsertPoint(entryBB.getTerminator());
+  // 设置初始值 - 确保entryBB有终止指令
+  if (Instruction *term = entryBB.getTerminator()) {
+    builder.SetInsertPoint(term);
+  } else {
+    builder.SetInsertPoint(&entryBB);
+  }
   builder.CreateStore(builder.getInt32(0), switchVar);
 
   // 创建调度块
@@ -41,14 +45,26 @@ PreservedAnalyses Flattening::run(Function &F, FunctionAnalysisManager &) {
     F.getContext(), "dispatchBB", &F);
   
   // 修改入口块的终止指令，跳转到调度块
-  BranchInst *newBr = BranchInst::Create(dispatchBB);
-  ReplaceInstWithInst(entryBB.getTerminator(), newBr);
+  if (Instruction *term = entryBB.getTerminator()) {
+    BranchInst *newBr = BranchInst::Create(dispatchBB);
+    ReplaceInstWithInst(term, newBr);
+  } else {
+    builder.SetInsertPoint(&entryBB);
+    builder.CreateBr(dispatchBB);
+  }
 
   // 创建默认块(用于返回)
   BasicBlock *defaultBB = BasicBlock::Create(
     F.getContext(), "defaultBB", &F);
   builder.SetInsertPoint(defaultBB);
-  builder.CreateRetVoid();
+  
+  // 保留原始返回指令的类型
+  if (F.getReturnType()->isVoidTy()) {
+    builder.CreateRetVoid();
+  } else {
+    // 对于非void返回类型，需要创建适当的默认返回值
+    builder.CreateRet(UndefValue::get(F.getReturnType()));
+  }
 
   // 创建switch指令
   builder.SetInsertPoint(dispatchBB);
@@ -62,23 +78,29 @@ PreservedAnalyses Flattening::run(Function &F, FunctionAnalysisManager &) {
     // 设置当前块的case值
     switchInst->addCase(builder.getInt32(i), bb);
 
+    // 跳过没有终止指令的基本块
+    if (!bb->getTerminator()) continue;
+
     // 修改终止指令
     if (BranchInst *br = dyn_cast<BranchInst>(bb->getTerminator())) {
+      builder.SetInsertPoint(br);
       if (br->isUnconditional()) {
         // 无条件跳转改为设置switch变量并跳回dispatch
-        builder.SetInsertPoint(br);
         builder.CreateStore(builder.getInt32(i + 1), switchVar);
         builder.CreateBr(dispatchBB);
         br->eraseFromParent();
       } else {
         // 条件跳转需要更复杂的处理(这里简化处理)
-        builder.SetInsertPoint(br);
+        // 可能需要创建PHI节点来正确处理条件分支
         builder.CreateStore(builder.getInt32(i + 1), switchVar);
         builder.CreateBr(dispatchBB);
         br->eraseFromParent();
       }
     } else if (isa<ReturnInst>(bb->getTerminator())) {
       // 返回指令保持不变
+      continue;
+    } else if (isa<UnreachableInst>(bb->getTerminator())) {
+      // 不可达指令保持不变
       continue;
     }
   }
