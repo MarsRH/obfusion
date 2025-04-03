c9f7ad879c690eff8c44cbe463be53955dc58d5d
WhitePeach <Elysia_awa@outlook.com>
2025-04-03 18:11:59 +0800

    🐞 fix:
    持续进行控制流平坦化算法的修复

--- src/Flattening.cpp
+++ src/Flattening.cpp
@@ -6,8 +6,8 @@ using std::vector;
 
 PreservedAnalyses Flattening::run(Function &F, FunctionAnalysisManager &) {
   errs() << "[Flattening] Pass running on " << F.getName() << "\n";
-  errs() << "[Flattening] Original IR:\n";
-  F.print(errs());
+  // errs() << "[Flattening] Original IR:\n";
+  // F.print(errs());
   if (flatten(&F)) {
     return PreservedAnalyses::none();
   }
@@ -68,18 +68,15 @@ bool Flattening::flatten(Function *f) {
   BasicBlock *loopEntry = BasicBlock::Create(f->getContext(), "loopEntry", f);
   BasicBlock *loopEnd = BasicBlock::Create(f->getContext(), "loopEnd", f);
   BasicBlock *swDefault = BasicBlock::Create(f->getContext(), "switchDefault", f);
-  
-  // 设置loopEnd块的终止指令
-  IRBuilder<> endBuilder(loopEnd);
-  endBuilder.CreateBr(loopEntry);
+
 
   // 创建调度变量
   IRBuilder<> entryBuilder(&entryBB);
-  entryBuilder.CreateBr(loopEntry);
   AllocaInst *switchVar = entryBuilder.CreateAlloca(
     entryBuilder.getInt32Ty(), nullptr, "switchVar");
   [[maybe_unused]] StoreInst *store = entryBuilder.CreateStore(
     entryBuilder.getInt32(0), switchVar);
+  entryBuilder.CreateBr(loopEntry);
 
   // 创建调度块
   IRBuilder<> dispatchBuilder(loopEntry);
@@ -89,7 +86,10 @@ bool Flattening::flatten(Function *f) {
 
   // 设置switchDefault块的终止指令
   IRBuilder<> defaultBuilder(swDefault);
-  defaultBuilder.CreateBr(loopEnd);
+  defaultBuilder.CreateBr(loopEnd);  
+  // 设置loopEnd块的终止指令
+  IRBuilder<> endBuilder(loopEnd);
+  endBuilder.CreateBr(loopEntry);
 
   // 将原始基本块添加到switch指令中
   int randNumCase = rand();
@@ -120,18 +120,25 @@ bool Flattening::flatten(Function *f) {
           randNumCase = rand();
         }
 
+        
         // 创建新的条件跳转指令
-        Value *cond = br->getCondition();
-        BranchInst::Create(bb->getTerminator()->getSuccessor(0), bb->getTerminator()->getSuccessor(1), cond, bb->getTerminator()->getIterator());
+        IRBuilder<> caseBuilder(bb);
+        Value *cond = caseBuilder.CreateSelect(
+          br->getCondition(), numCaseTrue, numCaseFalse, "cond" );
+        caseBuilder.CreateStore(cond, switchVar);
+        caseBuilder.CreateBr(loopEnd);
+        br->eraseFromParent();
+        // BranchInst::Create(bb->getTerminator()->getSuccessor(0), bb->getTerminator()->getSuccessor(1), cond, bb->getTerminator()->getIterator());
         // 创建新的switch case
-        switchI->addCase(numCaseTrue, bb->getTerminator()->getSuccessor(0));
-        switchI->addCase(numCaseFalse, bb->getTerminator()->getSuccessor(1));
+        // switchI->addCase(numCaseTrue, bb->getTerminator()->getSuccessor(0));
+        // switchI->addCase(numCaseFalse, bb->getTerminator()->getSuccessor(1));
       
       // 如果是无条件跳转
       } else {
         BasicBlock *succ = br->getSuccessor(0);
         br->eraseFromParent();
 
+        // 创建新的无条件跳转指令
         ConstantInt *numCase = switchI->findCaseDest(succ);
         if (numCase == nullptr) {
           numCase = dispatchBuilder.getInt32(randNumCase);
@@ -139,72 +146,15 @@ bool Flattening::flatten(Function *f) {
         }
 
         // 更新switch变量并跳转到循环结束
-        [[maybe_unused]] StoreInst *store = dispatchBuilder.CreateStore(numCase, switchVar);
-        BranchInst::Create(loopEnd, bb);
+        IRBuilder<> caseBuilder(bb);
+        caseBuilder.CreateStore(numCase, switchVar);
+        caseBuilder.CreateBr(loopEnd);
       }
     }
   }
 
-  // 修复栈帧
-  errs() << "[Flattening] Final IR before fixStack:\n";
+  errs() << "[Flattening] Final IR After Flattening:\n";
   f->print(errs());
-  // fixStack(*f);
 
   return true;
-}
-
-// 修复栈帧
-void Flattening::fixStack(Function &F) {
-  errs() << "[Flattening] Fixing stack for function: " << F.getName() << "\n";
-
-  vector<PHINode*> origPHI;
-  vector<Instruction*> origReg;
-  do{
-      origPHI.clear();
-      origReg.clear();
-      BasicBlock &entryBB = F.getEntryBlock();
-      for(BasicBlock &BB : F){
-          for(Instruction &I : BB){
-              if(PHINode *PN = dyn_cast<PHINode>(&I)){
-                  origPHI.push_back(PN);
-              }else if(!(isa<AllocaInst>(&I) && I.getParent() == &entryBB)
-                  && I.isUsedOutsideOfBlock(&BB)){
-                  origReg.push_back(&I);
-              }
-          }
-      }
-
-      errs() << "[Flattening] Found " << origPHI.size() << " PHI nodes and " 
-      << origReg.size() << " instructions to process\n";
-
-      for(PHINode *PN : origPHI){
-          // Skip PHI nodes that are already being processed
-          if(PN->getNumIncomingValues() == 0) continue;
-          
-          // Ensure all incoming blocks and values are valid
-          bool valid = true;
-          for(unsigned i = 0; i < PN->getNumIncomingValues(); ++i) {
-              if(!PN->getIncomingBlock(i) || !PN->getIncomingValue(i)) {
-                  errs() << "[Flattening] Warning: Invalid PHI node " << *PN 
-                         << " - skipping\n";
-                  valid = false;
-                  break;
-              }
-          }
-          if(valid) {
-              // Additional validation before demoting
-              if(PN->getParent() && !PN->getType()->isVoidTy()) {
-                  DemotePHIToStack(PN, entryBB.getTerminator()->getIterator());
-              } else {
-                  errs() << "[Flattening] Warning: Invalid PHI node context " 
-                         << *PN << " - skipping\n";
-              }
-          }
-      }
-      for(Instruction *I : origReg){
-          DemoteRegToStack(*I, false, entryBB.getTerminator()->getIterator());
-      }
-  }while(!origPHI.empty() || !origReg.empty());
-
-  errs() << "[Flattening] Finished fixing stack for function: " << F.getName() << "\n";
 }
