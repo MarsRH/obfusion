602f4ddb5e3e8da4452dec9f20f63bdf033815e8
WhitePeach <Elysia_awa@outlook.com>
2025-05-18 17:52:28 +0800

    ✨ feat(ConstantEncrypt): 添加整数和数组解密功能

--- include/OBFS/ConstantEncrypt.h
+++ include/OBFS/ConstantEncrypt.h
@@ -2,6 +2,12 @@
 
 #include "OBFS/Common.h"
 
+struct EncryptedGV {
+  llvm::GlobalVariable *GV;
+  uint64_t key;
+  uint32_t len;
+};
+
 namespace OBFS {
 
 // 字符串加密插件
@@ -9,15 +15,11 @@ class ConstantEncrypt : public llvm::PassInfoMixin<ConstantEncrypt> {
 public:
   llvm::PreservedAnalyses run(llvm::Module &M,
                              llvm::ModuleAnalysisManager &AM);
-  
-  static bool isRequired() { return true; }
 
-private:
-  // 字符串加密函数
-  std::string encryptString(StringRef Str);
-  
-  // 运行时解密函数
-  static char* decryptString(const char* encrypted, size_t length);
+  virtual void InsertIntDecryption(llvm::Module &M, EncryptedGV encGV, llvm::LLVMContext &ctx);
+  virtual void InsertArrayDecryption(llvm::Module &M, EncryptedGV encGV, llvm::LLVMContext &ctx);
+
+  static bool isRequired() { return true; }
 };
 
 } // namespace OBFS
