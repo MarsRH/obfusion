5eff8826564cb8a173d0904f26c5c1dbeef1edad
WhitePeach <Elysia_awa@outlook.com>
2025-06-04 15:17:21 +0800

    🐞 fix(Flattening.h): 移除不必要的头文件并整理构造函数

--- include/OBFS/Flattening.h
+++ include/OBFS/Flattening.h
@@ -2,18 +2,18 @@
 
 #include "OBFS/Common.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Transforms/Utils/LowerSwitch.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/Scalar/Reg2Mem.h"
-
-#include <cstdlib> // 包含 rand() 和 srand() 函数
-#include <ctime> // 包含 time() 函数
 
 namespace OBFS {
 
 // 控制流平坦化
 class Flattening : public llvm::PassInfoMixin<Flattening> {
 public:
+  Flattening();
+  
   llvm::PreservedAnalyses run(llvm::Function &F,
                              llvm::FunctionAnalysisManager &AM);
   
