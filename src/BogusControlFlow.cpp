8a9305f5b21c1c152233b0a7aa506b89e61472c8
WhitePeach <Elysia_awa@outlook.com>
2025-06-04 16:19:05 +0800

    🐞 fix(BogusControlFlow.cpp): 更新调试信息，优化虚假控制流生成逻辑并移除未使用的代码

--- src/BogusControlFlow.cpp
+++ src/BogusControlFlow.cpp
@@ -1,8 +1,29 @@
 #include "OBFS/BogusControlFlow.h"
+#include "OBFS/OpaquePredicates.h"
 
 using namespace llvm;
 using namespace OBFS;
 using std::vector;
+
+#define DEBUG_TYPE "BogusControlFlow"
+
+PreservedAnalyses BogusControlFlow::run(Function &F, FunctionAnalysisManager &AM) {
+  outs()<< "\n" << "[BogusControlFlow] Pass running on " << F.getName() << "\n";
+  // DEBUG: 打印原始IR
+  DEBUG_WITH_TYPE(DEBUG_TYPE, 
+    dbgs() << "[BogusControlFlow] Original IR:\n"; 
+    F.print(dbgs()););
+
+  if (!F.isDeclaration() && BogusCF(&F)) {
+    // DEBUG: 打印最终IR
+    DEBUG_WITH_TYPE(DEBUG_TYPE, 
+      dbgs() << "[BogusControlFlow] Final IR After Bogus Control Flow:\n"; 
+      F.print(dbgs()););
+  
+    return PreservedAnalyses::none();
+  }
+  return PreservedAnalyses::all();
+}
 
 bool BogusControlFlow::BogusCF(Function *f) {
   
@@ -34,9 +55,9 @@ bool BogusControlFlow::BogusCF(Function *f) {
     blocks.push_back(&BB);
   }
 
-  errs() << "[BogusControlFlow] Collect BB " << blocks.size() << "\n";
+  outs() << "[BogusControlFlow] Collected " << blocks.size() << " basic blocks" << "\n";
 
-  // 2. 为每个非入口基本块创建虚假分支
+  // 为每个非入口基本块创建虚假分支
   for (BasicBlock *BB : blocks) {
 
     // 获取原基本块终止指令
@@ -44,20 +65,21 @@ bool BogusControlFlow::BogusCF(Function *f) {
     Instruction *origTerm = BB->getTerminator();
 
     if (BranchInst *br = dyn_cast<BranchInst>(origTerm)) {
-      errs() << "[BogusControlFlow] BranchInst\n";
+      // dbgs() << "[BogusControlFlow] BranchInst\n";
       // 仅处理无条件分支，使用不透明谓词
       if (!br->isConditional()) {
-        errs() << "[BogusControlFlow] Not Conditional Br\n";
+        // dbgs() << "[BogusControlFlow] Not Conditional Br\n";
         // 创建不透明谓词
         IRBuilder<> builder(BB);
         if (Instruction *term = BB->getTerminator()) {
             builder.SetInsertPoint(term);
         }
-        Value *bogusCmp = createBogusCmp(builder);
-        errs() << "[BogusControlFlow] CreateBogusCMP ok\n";
+        Value *bogusCmp = OBFS::opaquepredicate->generate(builder);
+        // Value *bogusCmp = createBogusCmp(builder);
+        // dbgs() << "[BogusControlFlow] CreateBogusCMP ok\n";
         // 创建虚假分支 使用随机选择的基本块进行克隆
         BasicBlock *bogusBB = createCloneBasicBlock(blocks[rand() % blocks.size()]);
-        errs() << "[BogusControlFlow] CopyBB ok\n";
+        // dbgs() << "[BogusControlFlow] CopyBB ok\n";
         // 创建条件分支
         builder.CreateCondBr(bogusCmp, br->getSuccessor(0), bogusBB);
         origTerm->eraseFromParent();
@@ -66,23 +88,25 @@ bool BogusControlFlow::BogusCF(Function *f) {
     } 
     else if (SwitchInst *sw = dyn_cast<SwitchInst>(origTerm)) {
       // 为Switch添加不可达块
-      errs() << "[BogusControlFlow] SwitchInst\n";
+      // dbgs() << "[BogusControlFlow] SwitchInst\n";
       BasicBlock *bogusBB = createCloneBasicBlock(blocks[rand() % blocks.size()]);
       sw->addCase(ConstantInt::get(Type::getInt32Ty(f->getContext()), rand()), bogusBB);
     }
     else {
       // 其他情况使用默认处理 理论上不会到达该分支
-      errs() << "[BogusControlFlow] err branch\n";
+      // errs() << "[BogusControlFlow] err branch\n";
       IRBuilder<> builder(BB);
       if (Instruction *term = BB->getTerminator()) {
           builder.SetInsertPoint(term);
       }
-      Value *bogusCmp = createBogusCmp(builder);
+      Value *bogusCmp = OBFS::opaquepredicate->generate(builder);
+      // Value *bogusCmp = createBogusCmp(builder);
       BasicBlock *bogusBB = createCloneBasicBlock(blocks[rand() % blocks.size()]);
       builder.CreateCondBr(bogusCmp, bogusBB, BB->getNextNode());
     }
   }
 
+  outs() << "[BogusControlFlow] Bogus control flow added successfully\n";
   return true;
 }
 
@@ -112,39 +136,4 @@ BasicBlock* BogusControlFlow::createCloneBasicBlock(BasicBlock *BB) {
         }
     }
     return cloneBB;
-}
-
-Value* BogusControlFlow::createBogusCmp(IRBuilder<> &builder){
-    // if((y < 10 || x * (x + 1) % 2 == 0))
-    // 等价于 if(true)
-    Module *M = builder.GetInsertBlock()->getModule();
-    LLVMContext &Context = M->getContext();
-    
-    GlobalVariable *xptr = new GlobalVariable(*M, Type::getInt32Ty(Context), false, 
-        GlobalValue::CommonLinkage, ConstantInt::get(Type::getInt32Ty(Context), 0), "x");
-    GlobalVariable *yptr = new GlobalVariable(*M, Type::getInt32Ty(Context), false, 
-        GlobalValue::CommonLinkage, ConstantInt::get(Type::getInt32Ty(Context), 0), "y");
-    
-    Value *x = builder.CreateLoad(Type::getInt32Ty(Context), xptr);
-    Value *y = builder.CreateLoad(Type::getInt32Ty(Context), yptr);
-    Value *cond1 = builder.CreateICmpSLT(y, ConstantInt::get(Type::getInt32Ty(Context), 10));
-    Value *op1 = builder.CreateAdd(x, ConstantInt::get(Type::getInt32Ty(Context), 1));
-    Value *op2 = builder.CreateMul(op1, x);
-    Value *op3 = builder.CreateURem(op2, ConstantInt::get(Type::getInt32Ty(Context), 2));
-    Value *cond2 = builder.CreateICmpEQ(op3, ConstantInt::get(Type::getInt32Ty(Context), 0));
-    return builder.CreateOr(cond1, cond2);
-}
-
-PreservedAnalyses BogusControlFlow::run(Function &F, FunctionAnalysisManager &AM) {
-  errs() << "[BogusControlFlow] Pass running on " << F.getName() << "\n";
-  // DEBUG: 打印原始IR
-  errs() << "[BogusControlFlow] Original IR:\n";
-  F.print(errs());
-  if (!F.isDeclaration() && BogusCF(&F)) {
-    // DEBUG: 打印最终IR
-    errs() << "[BogusControlFlow] IR:\n";
-    F.print(errs());
-    return PreservedAnalyses::none();
-  }
-  return PreservedAnalyses::all();
 }
